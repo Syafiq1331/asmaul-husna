f5d28d434b839831b52b61e0d71b06dbc0334f01
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-02-05 14:46:32 +0700

    Fix line-height

--- pages/AsmaulHusna.js
+++ pages/AsmaulHusna.js
@@ -4,7 +4,7 @@ const AsmaulHusna = ({ name, translation, meaning }) => {
   return (
     <div className="card w-80 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
-        <h2 className='text-3xl'>{name}</h2>
+        <h2 className='text-3xl leading-10'>{name}</h2>
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{translation}</h2>
