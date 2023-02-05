5dd87457dcc3a0322c9d1990f17bf43cd1c15359
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-02-05 14:40:04 +0700

    Fix card width

--- pages/AsmaulHusna.js
+++ pages/AsmaulHusna.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AsmaulHusna = ({ name, translation, meaning }) => {
   return (
-    <div className="card min-w-80 bg-base-100 shadow-xl">
+    <div className="card w-80 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
         <h2 className='text-3xl'>{name}</h2>
       </figure>
